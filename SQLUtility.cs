9db685fe606ce044439b27a3509d18b69db12952
shiffygreen <shiffygreen73@gmail.com>
2024-06-19 17:49:41 -0400

    fixed sqlutility

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -39,6 +39,7 @@ namespace CPUFramework
                 try
                 {
                     SqlDataReader dr = cmd.ExecuteReader();
+                    CheckReturnValue(cmd);
                     if (loadtable == true)
                     {
                         dt.Load(dr);
@@ -56,6 +57,41 @@ namespace CPUFramework
             }
             SetAllColumnsAllowNull(dt);
             return dt;
+        }
+
+        private static void CheckReturnValue(SqlCommand cmd)
+        {
+            int returnvalue = 0;
+            string msg = "";
+            if (cmd.CommandType == CommandType.StoredProcedure)
+            {
+                foreach (SqlParameter p in cmd.Parameters)
+                {
+                    if (p.Direction == ParameterDirection.ReturnValue)
+                    {
+                        if (p.Value != null)
+                        {
+                            returnvalue = (int)p.Value;
+                        }
+                    }
+                    else if (p.ParameterName.ToLower() == "@message")
+                    {
+                        if (p.Value != null)
+                        {
+                            msg = p.Value.ToString();
+                        }
+                    }
+                }
+
+                if (returnvalue == 1)
+                {
+                    if (msg == "")
+                    {
+                        msg = $"{cmd.CommandText} did not do action that was requested";
+                    }
+                    throw new Exception(msg);
+                }
+            }
         }
 
         public static DataTable GetDataTable(string sqlstatement) //- take a SQL statement and return a DataTable
@@ -150,12 +186,6 @@ namespace CPUFramework
             return n;
         }
 
-        public static string GetFirstCOlumnFirstRowValueAsString(string sql)
-        {
-            DataTable dt = GetDataTable(sql);
-            sql = dt.Rows[0][0].ToString();
-            return sql.ToString();
-        }
 
         private static void SetAllColumnsAllowNull(DataTable dt)
         {
